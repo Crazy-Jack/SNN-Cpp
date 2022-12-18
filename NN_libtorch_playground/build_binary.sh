20347feff42855b61711b7a24c0daa8951954db6
Tianqin Li <jacklitianqin@gmail.com>
2022-12-18 02:56:51 -0500

    cleanup

--- NN_libtorch_playground/build_binary.sh
+++ NN_libtorch_playground/build_binary.sh
@@ -4,7 +4,7 @@ export CXX=`which g++`
 rm -rf build
 mkdir build
 cd build 
-cmake -DCMAKE_PREFIX_PATH=/home/tianqinl/SNN-Cpp/NN/libtorch ..
+cmake -DCMAKE_PREFIX_PATH=/home/tianqinl/SNN-Cpp/NN_libtorch_playground/libtorch ..
 cmake --build . --config Release
 cd ..
 
--- cache_replacement/build_binary.sh
+++ cache_replacement/build_binary.sh
@@ -4,7 +4,7 @@ export CXX=`which g++`
 rm -rf build
 mkdir build
 cd build 
-cmake -DCMAKE_PREFIX_PATH=/home/tianqinl/SNN-Cpp/src/libtorch ..
+cmake -DCMAKE_PREFIX_PATH=/home/tianqinl/SNN-Cpp/cache_replacement/libtorch ..
 cmake --build . --config Release
 cd ..
 
